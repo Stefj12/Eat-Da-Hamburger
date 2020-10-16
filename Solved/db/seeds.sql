989c1e763b87c3d627094c0867a4364fe7377c24
Code Boy <stefanjohnson650@gmail.com>
2020-10-15 18:47:49 -0700

    files for repo

---
+++ Solved/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Chicken burger');
+INSERT INTO burgers (name) VALUES ('Double cheese burger');
+INSERT INTO burgers (name, hungry) VALUES ('Triple cheese burger', true);
+INSERT INTO burgers (name, hungry) VALUES ('Quad burger', true);
+INSERT INTO burgers (name, hungry) VALUES ('Mega burger', true);
+INSERT INTO burgers (name) VALUES ('Burger');
